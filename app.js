2a6e5361b569fe5d4b662caec843277d55daa2fb
Heicha0103 <heichapozo@gmail.com>
2021-07-09 11:56:41 -0400

    Nuevo

--- app.js
+++ app.js
@@ -2,18 +2,36 @@ const {response} = require('express');
 const express = require('express');
 const app = express();
 
-const path = require('path');
+const port = 3000;
 
-app.listen(process.env.PORT || 3000)
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/imagenes', express.static(__dirname + 'public/imagenes'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/registro-form.html', (req, res)=>{
+    res.sendFile(__dirname + '/views/registro-form.html')
+})
+
+app.listen(port, ()=> console.info(`Escuchando en el puerto ${port}`));
+
+/*const path = require('path');
+
+app.listen(process.env.PORT || 3000)*/
 
 //app.use(express.static(path.join(__dirname ,'src/views')));
 
 //Start the sever
-app.get('/', function(req, res){
+/*app.get('/', function(req, res){
     
     console.log('Bienvenidos')
     res.sendFile(path.resolve(__dirname, 'src/views/index.html'))
 
 });
 
-module.exports = app;
+module.exports = app;*/
